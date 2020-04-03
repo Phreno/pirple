a47808d4c371d309e7d05fb9c2eb2d7c8135bd5b
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-03 12:09:50 +0000

    parsing query strings

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ const server = http.createServer((req, res) => {
   // get the path
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
-
   // get the HTTP method
   const method = req.method.toLowerCase()
-
+  // get the query string
+  const queryStringObject = parsedUrl.query
   // send the response
   res.end("hello\n")
   // log the request
-  console.log(`Request received on path: ${trimmedPath} with method: ${method}`)
+  console.log([
+    `Request received on path: ${trimmedPath}`,
+    `with method: ${method}`,
+    `and with these query string parameters ${JSON.stringify(queryStringObject)}`
+  ])
 })
 // start the server
 server.listen(3000, () => {
   console.log("start")
-
 })
