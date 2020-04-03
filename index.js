67ced654676a85279586ab4c4e8d133caf6f1734
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-03 12:15:16 +0000

    push

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const server = http.createServer((req, res) => {
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
   // get the HTTP method
   const method = req.method.toLowerCase()
+  // get the headers
+  const headers = req.headers
   // get the query string
   const queryStringObject = parsedUrl.query
   // send the response
   res.end("hello\n")
   // log the request
   console.log([
-    `Request received on path: ${trimmedPath}`,
-    `with method: ${method}`,
-    `and with these query string parameters ${JSON.stringify(queryStringObject)}`
-  ])
+    `request received on path: ${trimmedPath}`,
+    `method: ${method}`,
+    `query string parameters ${JSON.stringify(queryStringObject)}`,
+    `headers: ${JSON.stringify(headers, null, 2)}`
+  ].join("\n"))
 })
 // start the server
 server.listen(3000, () => {
