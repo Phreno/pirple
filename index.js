855a87a85d09d4335cd12a8590cf1f4d6224ffd1
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-03 11:58:09 +0000

    parsing http method

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const server = http.createServer((req, res) => {
   // get the path
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // get the HTTP method
+  const method = req.method.toLowerCase()
+
   // send the response
   res.end("hello\n")
   // log the request
-  console.log(`Request received on path: ${trimmedPath}`)
+  console.log(`Request received on path: ${trimmedPath} with method: ${method}`)
 })
 // start the server
 server.listen(3000, () => {
