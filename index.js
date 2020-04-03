80046fc89af541a4c83fff36cc4477a94f997f70
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-03 18:38:49 +0000

    parsing payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // the server respond
 const server = http.createServer((req, res) => {
@@ -12,17 +13,27 @@ const server = http.createServer((req, res) => {
   const method = req.method.toLowerCase()
   // get the headers
   const headers = req.headers
-  // get the query string
-  const queryStringObject = parsedUrl.query
-  // send the response
-  res.end("hello\n")
-  // log the request
-  console.log([
-    `request received on path: ${trimmedPath}`,
-    `method: ${method}`,
-    `query string parameters ${JSON.stringify(queryStringObject)}`,
-    `headers: ${JSON.stringify(headers, null, 2)}`
-  ].join("\n"))
+  // get the payload
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+  req.on('end', () => {
+    buffer += decoder.end()
+    // get the query string
+    const queryStringObject = parsedUrl.query
+    // send the response
+    res.end("hello\n")
+    // log the request
+    console.log([
+      `request received on path: ${trimmedPath}`,
+      `method: ${method}`,
+      `query string parameters ${JSON.stringify(queryStringObject)}`,
+      `headers: ${JSON.stringify(headers, null, 2)}`,
+      `payload: ${buffer}`
+    ].join("\n"))
+  })
 })
 // start the server
 server.listen(3000, () => {
