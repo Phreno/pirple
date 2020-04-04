0fd61236270127bc34822e4dd0a84db169876949
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-04 03:50:53 +0000

    storing data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
 const config = require('./config')
 const fs = require('fs')
+const _data = require('./lib/data')
 // create the http server
 const httpServer = http.createServer((req, res) => {
   unifiedServer(req, res)
