4b930cfd8924a12fc624d51231a092281a5614fd
K3rnelP4n1k <losgoomy@gmail.com>
2020-04-05 12:42:29 +0000

    add some script to fill data

---
+++ mock/create_dummy_users.sh
@@ -0,0 +1,14 @@
+for i in $(seq 1 20); 
+do 
+echo "creating user $i"
+curl --location --request POST "http://localhost:3000/users" \
+--header 'Content-Type: application/json' \
+--data-raw "{
+  \"firstName\": \"John\",
+  \"lastName\": \"Doe\",
+  \"phone\": \"00${i}\",
+  \"password\": \"@Bépo1234\",
+  \"tosAgreement\": true
+}"
+echo "... done"
+done
